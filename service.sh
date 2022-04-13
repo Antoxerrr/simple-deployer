df487a6d16812375a7bf12aa237db6691ebc6d7d
Antoxerrr <a.malyarov99@gmail.com>
2022-04-14 03:01:12 +1000

    init

--- service.sh
+++ service.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gunicorn -w 1 -b 0.0.0.0:9000 --capture-output --log-level debug --access-logfile /var/logs/gunicorn/deployer/access.log --error-logfile /var/logs/gunicorn/deployer/errors.log app:app
+mkdir /var/log/gunicorn/deployer
+gunicorn -w 1 -b 0.0.0.0:9000 --capture-output --log-level debug --access-logfile /var/log/gunicorn/deployer/deployer.access.log --error-logfile /var/log/gunicorn/deployer/deployer.errors.log app:app
