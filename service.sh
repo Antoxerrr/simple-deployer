965c2468da08e77ad05d2e14593c274271cf55f8
Antoxerrr <a.malyarov99@gmail.com>
2022-04-14 02:57:32 +1000

    init

--- service.sh
+++ service.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pipenv run gunicorn -w 1 -b 0.0.0.0:9000 --capture-output --log-level debug --access-logfile /root/apps/simple-deployer/access.log --error-logfile /root/apps/simple-deployer/errors.log app:app
+pipenv run gunicorn -w 1 -b 0.0.0.0:9000 --capture-output --log-level debug --access-logfile /var/logs/gunicorn/deployer/access.log --error-logfile /var/logs/gunicorn/deployer/errors.log app:app
