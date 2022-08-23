59abf0d368d87f8095b265bc6e9dc30f517d2a05
Antoxerrr <a.malyarov99@gmail.com>
2022-08-23 14:20:10 +1000

    Пересборка celery

--- deployer/update.sh
+++ deployer/update.sh
@@ -6,7 +6,7 @@ if ! cd "$1" ; then
 fi
 
 git pull origin $2
-docker-compose rm -sf app
-docker-compose build --no-cache app
-docker-compose up -d app
+docker-compose rm -sf celery_worker celery_beat app
+docker-compose build --no-cache celery_worker celery_beat app
+docker-compose up -d celery_worker celery_beat app
 docker system prune -af
